94b5b971f54b02bb4959e74260699ecfa85bdb2a
malkoG <rijgndqw012@gmail.com>
2025-10-20 21:00:10 +0900

    Complete chapter 1

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from browser-study!")
+import sys
+from url import URL
+
+
+def show(body):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+def load(url):
+    body = url.request()
+    show(body)
 
 
 if __name__ == "__main__":
-    main()
+    load(URL(sys.argv[1]))
