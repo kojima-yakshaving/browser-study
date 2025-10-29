e224cbf2e4d0d3a8c4c11466b2f3d120d32a4f3e
malkoG <rijgndqw012@gmail.com>
2025-10-29 14:56:32 +0900

    Handle 'about:blank' and improve URL parsing error management

--- gorushi/url.py
+++ gorushi/url.py
@@ -32,53 +32,70 @@ class URL:
                 view_source=view_source
             )
 
-        tokens = url.split("://", 1)
-        if len(tokens) == 1:
-            # if scheme is not specified and starts with "/", assume file scheme
-            if url.startswith("/"):
-                scheme = "file"
+        if url == "about:blank":
+            return cls(
+                scheme="about",
+                path=":blank",
+                host="",
+                port=0,
+                view_source=view_source
+            )
+    
+        try:
+            tokens = url.split("://", 1)
+            if len(tokens) == 1:
+                # if scheme is not specified and starts with "/", assume file scheme
+                if url.startswith("/"):
+                    scheme = "file"
+                else:
+                    raise ValueError("Invalid URL: {}".format(url))
             else:
-                raise ValueError("Invalid URL: {}".format(url))
-        else:
-            scheme, url = url.split("://", 1)
+                scheme, url = url.split("://", 1)
 
-            assert scheme in ("http", "https", "file")
+                assert scheme in ("http", "https", "file")
 
-        if scheme == "file":
-            if url.startswith("//"):
-                url = url[2:]
-            # if url not starts with "/", add it
-            if not url.startswith("/"):
-                url = "/" + url
-            path = url
+            if scheme == "file":
+                if url.startswith("//"):
+                    url = url[2:]
+                # if url not starts with "/", add it
+                if not url.startswith("/"):
+                    url = "/" + url
+                path = url
+                return cls(
+                    scheme=scheme, 
+                    path=path, 
+                    host="", 
+                    port=0, 
+                    view_source=view_source
+                )
+
+            port = 0
+            if "/" not in url:
+                url = url + "/"
+            host, url = url.split("/", 1)
+            path = "/" + url
+
+            if scheme == "http":
+                port = 80
+            elif scheme == "https":
+                port = 443
+
+            if ":" in host:
+                host, port = host.split(":", 1)
+                port = int(port)
+
             return cls(
                 scheme=scheme, 
                 path=path, 
-                host="", 
-                port=0, 
+                host=host, 
+                port=port, 
                 view_source=view_source
             )
-
-        port = 0
-        if "/" not in url:
-            url = url + "/"
-        host, url = url.split("/", 1)
-        path = "/" + url
-
-        if scheme == "http":
-            port = 80
-        elif scheme == "https":
-            port = 443
-
-        if ":" in host:
-            host, port = host.split(":", 1)
-            port = int(port)
-
-        return cls(
-            scheme=scheme, 
-            path=path, 
-            host=host, 
-            port=port, 
-            view_source=view_source
-        )
+        except Exception as _:
+            return cls(
+                scheme="about",
+                path=":blank",
+                host="",
+                port=0,
+            )
         
